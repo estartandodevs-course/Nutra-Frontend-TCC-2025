e27fc4268b6356e82bf31d3e59f9b00c4d6e132c
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-05 22:21:31 -0300

    feat(component): add wrapper component

---
+++ src/components/wrapper/Wrapper.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface WrapperProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Wrapper({ children, className = "" }: WrapperProps) {
+  return (
+    <div
+      className={`mx-auto max-w-lg min-h-screen bg-gray-50 p-4 shadow-xl md:max-w-xl lg:max-w-2xl ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
