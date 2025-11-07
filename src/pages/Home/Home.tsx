469dc3ba99238c62986f0e5c13e883a8dfbc5f51
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-06 23:32:50 -0300

    feat(component): add progressbar component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,16 @@
-import  Wrapper from "../../components/wrapper/Wrapper";
+import Wrapper from "../../components/wrapper/Wrapper";
+import ProgressBar from "../../components/quiz/Progressbar";
 
-function Home() {
+export default function Home() {
   return (
     <Wrapper>
+      <ProgressBar current={2} total={5} />
       <section className="flex flex-col items-center justify-center text-center min-h-screen">
-        <h1 className="text-2xl font-semibold text-gray-800 mb-2">Nutra</h1>
-        <p className="text-gray-500 text-sm">
+        <h1 className="text-2xl font-semibold text-gray-800 mb-4">Nutra</h1>
+        <p className="text-gray-500 text-sm mb-6">
           Bem-vindo ao app Nutra!
         </p>
       </section>
     </Wrapper>
   );
 }
-
-export default Home;
