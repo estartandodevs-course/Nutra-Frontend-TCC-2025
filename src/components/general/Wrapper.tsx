c33e8e453ab443595fe929e72dcdab6e953f3cd8
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-10 23:20:41 -0300

    fix(wrapper): fix wrapper layout

--- src/components/general/Wrapper.tsx
+++ src/components/general/Wrapper.tsx
@@ -2,8 +2,8 @@ import type { WrapperProps } from "../../types/wrapper";
 
 export default function MobileWrapper({ children }: WrapperProps) {
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center px-4 sm:px-6 lg:px-8">
-      <div className="relative w-full max-w-md bg-white rounded-2xl shadow-md flex flex-col h-auto max-h-screen overflow-y-auto">
+    <div className="min-h-screen bg-gray-100 flex justify-center">
+      <div className="relative w-full max-w-md bg-white shadow-md flex flex-col h-auto max-h-screen overflow-y-auto rounded-none lg:rounded-2xl">
         {children}
       </div>
     </div>
