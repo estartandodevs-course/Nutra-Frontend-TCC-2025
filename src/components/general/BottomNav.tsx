dc030356aced98e04ae3076f594157be1479d90b
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-13 22:26:20 -0300

    fix(page): fix ui AchievementsPage

--- src/components/general/BottomNav.tsx
+++ src/components/general/BottomNav.tsx
@@ -2,29 +2,30 @@ import { Home, User, Leaf, Menu } from "lucide-react";
 
 export default function BottomNav() {
   return (
-    <nav className="sticky bottom-0 left-0 w-full bg-orange-500 border-t border-orange-600 z-10">
-      <div className="flex justify-around items-center h-16">
-        <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
-          <Home size={24} />
-          <span className="font-fredoka text-[16px]">Inicio</span>
-        </button>
+    <nav className="fixed bottom-0 left-0 w-full flex justify-center z-50">
+      <div className="w-full max-w-md bg-orange-500 border-t border-orange-600">
+        <div className="flex justify-around items-center h-16">
+          <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
+            <Home size={24} />
+            <span className="font-fredoka text-[16px]">Inicio</span>
+          </button>
 
-        <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
-          <User size={24} />
-          <span className="font-fredoka text-[16px]">Meu Perfil</span>
-        </button>
+          <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
+            <User size={24} />
+            <span className="font-fredoka text-[16px]">Meu Perfil</span>
+          </button>
 
-        <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
-          <Leaf size={24} />
-          <span className="font-fredoka text-[16px]">Minha Horta</span>
-        </button>
+          <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
+            <Leaf size={24} />
+            <span className="font-fredoka text-[16px]">Minha Horta</span>
+          </button>
 
-        <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
-          <Menu size={24} />
-          <span className="font-fredoka text-[16px]">Menu</span>
-        </button>
+          <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
+            <Menu size={24} />
+            <span className="font-fredoka text-[16px]">Menu</span>
+          </button>
+        </div>
       </div>
     </nav>
   );
 }
-
--- src/pages/ProgressPage/ProgressPage.tsx
+++ src/pages/ProgressPage/ProgressPage.tsx
@@ -9,18 +9,15 @@ export default function ProgressPage() {
     <Wrapper>
       <Header />
 
-      <div className="flex-1 overflow-y-auto pb-20">
-        <div className="px-4 py-4">
-          <h1 className="text-xl font-bold text-gray-900">Seu progresso</h1>
-        </div>
-
-        <div className="px-4 pb-6 space-y-4 max-w-md mx-auto">
+      <main className="flex-1 overflow-y-auto px-4 py-6 pb-20">
+        <div className="max-w-md mx-auto">
           <ProgressCard />
           <TaskList />
         </div>
-      </div>
+      </main>
 
       <BottomNav />
     </Wrapper>
-  )
+  );
 }
+
