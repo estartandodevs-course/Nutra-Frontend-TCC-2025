d42283af4c93cfc8a49ab79a39d024f3f8c31e6b
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-13 22:33:14 -0300

    fix(component): fix nowrap Bottomnav

--- src/components/general/BottomNav.tsx
+++ src/components/general/BottomNav.tsx
@@ -7,22 +7,22 @@ export default function BottomNav() {
         <div className="flex justify-around items-center h-16">
           <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
             <Home size={24} />
-            <span className="font-fredoka text-[16px]">Inicio</span>
+            <span className="font-fredoka text-[16px] whitespace-nowrap">Inicio</span>
           </button>
 
           <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
             <User size={24} />
-            <span className="font-fredoka text-[16px]">Meu Perfil</span>
+            <span className="font-fredoka text-[16px] whitespace-nowrap">Meu Perfil</span>
           </button>
 
           <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
             <Leaf size={24} />
-            <span className="font-fredoka text-[16px]">Minha Horta</span>
+            <span className="font-fredoka text-[16px] whitespace-nowrap">Minha Horta</span>
           </button>
 
           <button className="flex flex-col items-center justify-center w-16 h-16 text-white">
             <Menu size={24} />
-            <span className="font-fredoka text-[16px]">Menu</span>
+            <span className="font-fredoka text-[16px] whitespace-nowrap">Menu</span>
           </button>
         </div>
       </div>
