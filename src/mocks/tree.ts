37233be5396baa8e4589cdabe033fb6c7e33046f
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-14 23:42:24 -0300

    refactor(mocks): move SHOP_ITEMS into mocks folder

---
+++ src/mocks/tree.ts
@@ -0,0 +1,8 @@
+export const SHOP_ITEMS = [
+  { id: 1, name: "Maçã", emoji: "🍎", cost: 100 },
+  { id: 2, name: "Melancia", emoji: "🍉", cost: 150 },
+  { id: 3, name: "Moeda", emoji: "🪙", cost: 220 },
+  { id: 4, name: "Melancia", emoji: "🍌", cost: 270 },
+  { id: 5, name: "Banana", emoji: "🍌", cost: 320 },
+  { id: 6, name: "Mirtilo", emoji: "🫐", cost: 360 },
+]
