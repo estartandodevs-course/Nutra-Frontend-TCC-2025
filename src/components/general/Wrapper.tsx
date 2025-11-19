e680cde500054e34f7ed92871c442d7c42d5d97d
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-18 22:53:14 -0300

    fix(component): fix Wrapper container ui

--- src/components/general/Wrapper.tsx
+++ src/components/general/Wrapper.tsx
@@ -3,7 +3,7 @@ import type { WrapperProps } from "../../types/wrapper";
 export default function MobileWrapper({ children }: WrapperProps) {
   return (
     <div className="h-screen bg-gray-100 flex justify-center">
-      <div className="w-full max-w-md bg-white shadow-md flex flex-col h-full rounded-none lg:rounded-2xl">
+      <div className="w-full max-w-md bg-white shadow-md flex flex-col h-full rounded-none rounded-2xl">
         <div className="flex-1 overflow-auto">
           {children}
         </div>
