b78923ed2bcf2bac27b2d800a9b0d6a3547d2946
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-21 20:00:48 -0300

    fix(component): fix splash component  responsiviness

--- src/components/Splashscreen/Splash.tsx
+++ src/components/Splashscreen/Splash.tsx
@@ -4,18 +4,24 @@ import Cajuzao from "../../assets/images/splash/cajuzao.png"
 export default function SplashScreen() {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center relative bg-white overflow-hidden">
+
       <div className="flex flex-col items-center justify-center z-10 mb-40">
         <img
           src={BigNutraLogo}
           alt="Logo Nutra"
-          className="w-98 h-auto"
+          className="w-full max-w-[392px] h-auto object-contain"
         />
       </div>
 
       <img
         src={Cajuzao}
         alt="Caju mascote"
-        className="absolute bottom-0 left-0 w-full h-1/2 object-cover z-0"
+        className="
+          absolute bottom-0 left-1/2 -translate-x-1/2
+          w-full max-w-[600px]
+          h-auto object-contain
+          z-0
+        "
       />
     </div>
   )
