30dfecc7dd6ef83a923a000cff14b767f4c414eb
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-22 19:39:02 -0300

    update(splash): insert comment lines to deactivate route

--- src/pages/SplashPage/SplashPage.tsx
+++ src/pages/SplashPage/SplashPage.tsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 import Wrapper from "../../components/General/Wrapper";
 import SplashScreen from "../../components/Splashscreen/Splash";
 
 export default function SplashPage() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate("/welcome"); 
-    }, 8000); 
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     navigate("/welcome"); 
+  // //   }, 6000); 
 
-    return () => clearTimeout(timer); 
-  }, [navigate]);
+  //   return () => clearTimeout(timer); 
+  // }, [navigate]);
   return (
     <Wrapper>
       <SplashScreen />
