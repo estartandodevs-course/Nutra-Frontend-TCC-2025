2e7136c7faf26581427d0a1b18f7a9e95944cecc
Bruno Fabbio <brunobiodev@gmail.com>
2025-11-27 13:57:37 -0300

    fix(ci-cd): fix CD deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,23 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1200,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          router: ["react-router-dom"],
-          motion: ["framer-motion"],
-          forms: ["react-hook-form"], 
-        },
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1200,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          router: ["react-router-dom"],
+          motion: ["framer-motion"],
+          forms: ["react-hook-form"], 
+        },
+      },
+    },
+  },
 });
